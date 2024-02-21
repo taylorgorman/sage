f9df65e56fc123c9a398f53b2b5bbfae89f027b7
Taylor Gorman <taylor@thegorman.group>
2024-02-21 11:18:39 -0600

    Use wp_nav_menu to generate more HTML

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -3,9 +3,15 @@
     {!! $siteName !!}
   </a>
 
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
+  @php($wp_nav_menu_name = 'primary_navigation')
+  @if (has_nav_menu($wp_nav_menu_name))
+    {!! wp_nav_menu([
+      'theme_location' => $wp_nav_menu_name,
+      'container' => 'nav',
+      'container_class' => $wp_nav_menu_name,
+      'container_aria_label' => wp_get_nav_menu_name($wp_nav_menu_name),
+      'menu_class' => 'nav',
+      'echo' => false,
+    ]) !!}
   @endif
 </header>
